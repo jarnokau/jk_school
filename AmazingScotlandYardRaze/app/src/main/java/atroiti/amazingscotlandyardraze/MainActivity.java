304ece70bc9c42fadf4f59a7cb3803b6467a4375
jarnokau <jarno.kaunisto@gmail.com>
2017-05-04 16:34:50 +0300

    changed application to use viewpager. settings could be changed to use SharedPreference instead of file saving.

--- AmazingScotlandYardRaze/app/src/main/java/atroiti/amazingscotlandyardraze/MainActivity.java
+++ AmazingScotlandYardRaze/app/src/main/java/atroiti/amazingscotlandyardraze/MainActivity.java
@@ -1,19 +1,26 @@
 package atroiti.amazingscotlandyardraze;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.String;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button RobberButton,PoliceButton;
+    Button ContinueButton, EndGameButton;
+    Boolean newGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +28,43 @@ public class MainActivity extends AppCompatActivity {
         String path = String.valueOf(getFilesDir());
         String filename = "gameFile.cfg";
         Boolean fileExists;
+        // check is there game file present
         fileExists = fileExistance(filename);
         if(fileExists) {
-            // if file exists
-            //read file information
+            newGame = false;
+            // if file exist, read information from it(is player robber/police etc) then later on based on that open correct view
+
+            //read rows from given file
             //first row contains role: Robber or Police
             //second row contains contact information of another player
-            setContentView(R.layout.activity_main);
+
             //inside activity_main open content_main2 = continue or not
             //this happens only when starting application
 
             //if continue open role related view
             //if does not continue, delete game file and open new game view
         }else{
+            newGame = true;
+        }
             //if file does not exists, open "new game view"
+
             setContentView(R.layout.activity_main);
+            RobberButton = (Button)(findViewById(R.id.Location1_button));
+            RobberButton.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View view) {
+
+                                                }
+                                            });
             // inside activity_main open content_main = new game view
             //
-        }
 
-        // check is there game file present
-        // if file exist, read information from it(is player robber/police etc) then later on based on that open correct view
-        // if file does not exist, first screen will be "new game"
-        // game file will be removed when game ends
 
-        setContentView(R.layout.activity_main);
+
+
+        // game file will be removed when game ends (using "end game" button)
+
+        //setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -87,26 +106,35 @@ public class MainActivity extends AppCompatActivity {
         return deleted;
     }
 
-    public boolean createAndWriteFile(String fname, String Role, String OtherPlayer) throws IOException {
-        File dir = getFilesDir();
-        File file = new File(dir, fname);
-        FileOutputStream stream = new FileOutputStream(fname);
+    public boolean createAndWriteFile(String fname, String Role, String OtherPlayer) {
+        //File dir = getFilesDir();
+        //File file = new File(dir, fname);
+        //FileOutputStream stream = new FileOutputStream(fname);
+        //String filename = "myfile";
+        //String string = "Hello world!";
+
+        //write are you Robber or Police
+        String string = Role + "\n\r";
+        //write contact information of other player
+        string = string + OtherPlayer + "\n\r";
+
+        FileOutputStream outputStream;
+
         try {
-            //write are you Robber or Police
-            stream.write(Role.getBytes());
-            //write contact information of other player
-            stream.write(OtherPlayer.getBytes());
-        } finally {
-            stream.close();
+            outputStream = openFileOutput(fname, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
         //change this to return was read succesful or not
         return true;
     }
 
     public Boolean readFile(String fname){
-        //read rows from given file
-        //first row contains role
-        // second row contains contact infromation to other player
+
         //change so it will return readed information, also change funktion type from Boolean to correct one
         return true;
     };
